95014c9726f6350f7bd7d82b3333996dfb8ac0b9
jeong922 <hwajeong922@naver.com>
2023-05-30 23:22:20 +0900

    ItemComponent 추가

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,25 @@
 import { VideoComponent } from './components/page/item/videoComponent.js';
 import { ImageComponent } from './components/page/item/imageComponent.js';
 import { NoteComponent } from './components/page/item/noteComponent.js';
-import { PageComponent } from './components/page/pageComponent.js';
+import { Composable, PageComponent } from './components/page/pageComponent.js';
 import { TodoComponent } from './components/page/item/todoComponent.js';
+import { Component } from './components/component';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(root: HTMLElement) {
+    // 이렇게 바로 생성하는 것보다 dependency injection을 사용하는 것이 좋음
     this.page = new PageComponent();
     this.page.attachTo(root);
 
     const image = new ImageComponent('image title', '../assets/image.jpg');
-    image.attachTo(root, 'beforeend');
+    this.page.addchild(image);
     const note = new NoteComponent('note title', 'note text');
-    note.attachTo(root, 'beforeend');
+    this.page.addchild(note);
     const todo = new TodoComponent('todo title', 'todo text');
-    todo.attachTo(root, 'beforeend');
+    this.page.addchild(todo);
     const video = new VideoComponent('춘식', 'https://youtu.be/nqJaKJYQXVU');
-    video.attachTo(root, 'beforeend');
+    this.page.addchild(video);
   }
 }
 
--- src/components/page/pageComponent.ts
+++ src/components/page/pageComponent.ts
@@ -1,7 +1,41 @@
-import { BaseComponent } from '../component.js';
-
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+import { BaseComponent, Component } from '../component.js';
+// interface를 따로 만들어준 이유
+// -> coupling 때문!
+//  클래스들 간에 서로 커플링이 심하게 되어 있으면 유지보수성, 확정성이 떨어지기 때문
+//  클래스에서 주된 규격 사항들을 인터페이스로 정의해 놓고 사용하는 곳에서 인터페이스 이름의 타입으로 지정해 두면
+//  다른 구현사항이 생기면 쉽게 다른 클래스로 변환 가능
+export interface Composable {
+  addchild(chile: Component): void;
+}
+// ItemComponent가 필요한 이유 : 재사용성 때문!
+// 나중에 버튼이 필요 없는 경우가 있을때 조건문을 만들거나 하지 않고 컴포넌트를 재사용 가능
+class ItemComponent extends BaseComponent<HTMLElement> implements Composable {
   constructor() {
-    super('<ul class="page">This is pageComponent</ul>');
+    super(`<li class="page-item">
+            <section class="page-item__body">
+              <button class="page-item__controls">✖</button>
+            </section>
+          </li>`);
+  }
+  addchild(child: Component) {
+    const container = this.element.querySelector(
+      '.page-item__body'
+    )! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
+  constructor() {
+    super('<ul class="page"></ul>');
+  }
+
+  addchild(section: Component) {
+    const item = new ItemComponent();
+    item.addchild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 }
